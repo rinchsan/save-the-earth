583d63b8b12bc9f716b7ff0b654b832a99b50a3c
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-07-29 07:27:18 +0900

    Add spaceship

--- save-the-earth/GameScene.swift
+++ save-the-earth/GameScene.swift
@@ -12,6 +12,7 @@ import GameplayKit
 class GameScene: SKScene, SKPhysicsContactDelegate {
 
     var earth: SKSpriteNode!
+    var spaceship: SKSpriteNode!
 
     override func didMove(to view: SKView) {
         self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
@@ -21,6 +22,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         earth.xScale = 2
         earth.yScale = 0.5
         earth.position = CGPoint(x: frame.width / 2, y: 0)
+
+        spaceship = SKSpriteNode(imageNamed: "spaceship")
+        spaceship.scale(to: CGSize(width: frame.width / 5, height: frame.width / 5))
+        spaceship.position = CGPoint(x: frame.width / 2, y: earth.frame.maxY + 50)
+        addChild(spaceship)
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
